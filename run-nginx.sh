2d7772dae598394d058bdc616a2b54ea85e39ecb
vrapracash <veeraprakash.bg@gmail.com>
2025-04-07 23:01:53 +0530

    docker-add

--- run-nginx.sh
+++ run-nginx.sh
@@ -1,2 +1,5 @@
-docker run nginx
-docker run mysql
+docker run nginx -d -p 8080:80
+# docker run mysql -d -p 8081:80
+cd /usr/share/nginx/html
+rm -rf index.html
+ echo "<h1> I am from NGINX container </h1>"
